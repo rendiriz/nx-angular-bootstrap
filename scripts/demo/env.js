72e5f7dcdd26db998405eebe66e883fa32cbffd6
Rendi Rizki <rendirizkir@gmail.com>
2022-05-10 20:12:30 +0700

    feat(demo): dynamic environment

---
+++ scripts/demo/env.js
@@ -0,0 +1,29 @@
+const { writeFile } = require('fs');
+
+require('dotenv').config();
+
+const environment = process.env.ENVIRONMENT;
+const isProduction = environment === 'production';
+
+let targetPath = './apps/demo/src/environments/';
+if (environment === 'development') {
+  targetPath += `environment.dev.ts`;
+} else if (environment === 'production') {
+  targetPath += `environment.prod.ts`;
+} else {
+  targetPath += `environment.ts`;
+}
+
+const fileContent = `
+export const environment = {
+  environment: '${environment}',
+  production: ${isProduction},
+};
+`;
+
+writeFile(targetPath, fileContent, (err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.info(`Wrote variables to ${targetPath}`);
+});
