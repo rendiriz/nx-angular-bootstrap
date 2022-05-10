2810c2e0d85e9c9dba5ec5c8bf5110c1d938d2f8
Rendi Rizki <rendirizkir@gmail.com>
2022-05-10 20:01:21 +0700

    feat(demo): setup

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-bootstrap-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
