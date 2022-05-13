27a57d2a45a14452516d87cc8bdf2b7a6dcd545a
Rendi Rizki <rendirizkir@gmail.com>
2022-05-13 17:56:33 +0700

    feat: input select

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -19,6 +19,27 @@ export class AppComponent {
   closeResult = '';
   active = 1;
 
+  simpleItems: any[] = [];
+  selectedSimpleItem = 1;
+  dropdownItems: any[] = [];
+  selectedDropdownItem = 1;
+  longItems: any[] = [];
+  selectedLongItem = 1;
+  headerItems: any[] = [];
+  selectedHeaderItem = 1;
+  searchItems: any[] = [];
+  selectedSearchItem = 1;
+  radioItems: any[] = [];
+  selectedRadioItem = 'Indonesia';
+  multiItems: any[] = [];
+  selectedMultiItem = [1, 2];
+  multiMoreItems: any[] = [];
+  selectedMultiMoreItem = [1, 2, 3, 4];
+  multiGroupItems: any[] = [];
+  selectedMultiGroupItem = [1, 2, 3, 4];
+  multiCheckboxItems: any[] = [];
+  selectedMultiCheckboxItem = [1, 2, 3, 4];
+
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private modalService: NgbModal
@@ -27,6 +48,46 @@ export class AppComponent {
     console.error(map([{ id: 'foo' }, { id: 'bar' }], (obj) => obj.id));
     console.error(moment().format('YYYY-MM-DD'));
     this.loadStyle('green');
+
+    const option = [
+      { value: 1, label: 'Indonesia' },
+      { value: 2, label: 'Inggris' },
+      { value: 3, label: 'Amerika' },
+      { value: 4, label: 'Rusia' },
+      { value: 5, label: 'Korea Utara' },
+      { value: 6, label: 'Korea Selatan' },
+      { value: 7, label: 'Malaysia' },
+      { value: 8, label: 'Singapura' },
+      { value: 9, label: 'Australia' },
+      { value: 10, label: 'Gabon' },
+      { value: 11, label: 'Jerman' },
+      { value: 12, label: 'Portugal' },
+      { value: 13, label: 'Rumania' },
+      { value: 14, label: 'Selandia Baru' },
+      { value: 15, label: 'Myanmar' },
+      {
+        value: 16,
+        label:
+          'Synergistically procrastinate distributed infrastructures for mission-critical expertise. Appropriately customize innovative innovation with resource sucking portals. Interactively customize diverse products for strategic applications.',
+      },
+    ];
+
+    this.simpleItems = option;
+    this.dropdownItems = option;
+    this.longItems = option;
+    this.headerItems = option;
+    this.searchItems = option;
+
+    const optionRadio = [
+      { value: 'Indonesia', label: 'Indonesia' },
+      { value: 'Inggris', label: 'Inggris' },
+    ];
+    this.radioItems = optionRadio;
+
+    this.multiItems = option;
+    this.multiMoreItems = option;
+    this.multiGroupItems = option;
+    this.multiCheckboxItems = option;
   }
 
   loadStyle(styleName: string) {
--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    FormsModule,
     NgbModule,
+    NgSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
